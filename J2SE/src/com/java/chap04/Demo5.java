bb9376d62a9b428376dd0346cce37f85db9887d8
yanxin152133 <yanxin152133@gmail.com>
2019-07-16 14:52:34 +0800

    java运算符

---
+++ J2SE/src/com/java/chap04/Demo5.java
@@ -0,0 +1,31 @@
+package com.java.chap04;
+
+/**
+ * @author Yan
+ * @date 2019/7/16 14:22
+ */
+public class Demo5 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+
+
+        // > 大于
+        System.out.println(a + ">" + b + ":" + (a > b));
+
+        // < 小于
+        System.out.println(a + "<" + b + ":" + (a < b));
+
+        // >= 大于等于
+        System.out.println(a + ">=" + b + ":" + (a >= b));
+
+        // <= 小于等于
+        System.out.println(a + "<=" + b + ":" + (a <= b));
+
+        // == 等于
+        System.out.println(a + "==" + b + ":" + (a == b));
+
+        // != 不等于
+        System.out.println(a + "!=" + b + ":" + (a != b));
+    }
+}
---
+++ J2SE/src/com/java/chap04/Demo6.java
@@ -0,0 +1,13 @@
+package com.java.chap04;
+
+/**
+ * @author Yan
+ * @date 2019/7/16 14:26
+ */
+public class Demo6 {
+    public static void main(String[] args) {
+        //三目运算符
+        String s=2>3?"表达式为真":"表达式为假";
+        System.out.println("s = " + s);
+    }
+}
