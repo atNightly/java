9347da3470f3ba4f0e8714c4a9b9f471c56f8b1b
yanxin152133 <yanxin152133@gmail.com>
2019-07-22 14:17:41 +0800

    Java常用类

---
+++ J2SE/src/com/java/chap09/sec03/TestMath.java
@@ -0,0 +1,14 @@
+package com.java.chap09.sec03;
+
+/**
+ * @author Yan
+ * @date 2019/7/22 14:10
+ */
+public class TestMath {
+    public static void main(String[] args) {
+        System.out.println("最大值："+Math.max(1,2));
+        System.out.println("最小值："+Math.min(1,2));
+        System.out.println("四舍五入："+Math.round(12.4));
+        System.out.println("次幂："+Math.pow(3,4));
+    }
+}
